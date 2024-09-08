8bdbf313dc4f24c1c156b8a776892bbd399dbbdb
Chonwai <chonwaiun@gmail.com>
2024-09-09 02:18:15 +0800

    Add: Added the async.

--- api/index.py
+++ api/index.py
@@ -20,17 +20,17 @@ def predicting(smiles_list, model_path):
 
     # 使用 `load_data` 加載數據
     test_data = load_data(smiles_list, args, from_file=False)
-    
+
     # 加載模型和進行預測
     scaler = get_scaler(model_path)
     model = load_model(model_path, args.cuda)
-    
+
     # 預測
     test_pred = predict(model, test_data, args.batch_size, scaler)
-    
+
     # 將結果轉換為 Python 列表
     test_pred = np.array(test_pred).tolist()
-    
+
     # 返回預測結果
     return test_pred, test_data.smile()
 
@@ -40,25 +40,23 @@ def hello_world():
     return {"message": "Hello World"}
 
 
-"""
-分子毒性预测接口，接受 SMILES 列表，并返回对应的预测值。
-
-:param request: 包含 SMILES 列表和模型类型的 JSON 请求。
-:return: 返回预测结果的 JSON 格式。
-"""
 @app.post("/api/predict")
-def predict_toxicity(request: PredictionRequest):
+async def predict_toxicity(request: PredictionRequest):
     # 根據 model_type 選擇對應的模型路徑
-    model_map = {"F2F": "service/model/F2F.pt", "C2C": "service/model/C2C.pt", "A2A": "service/model/A2A.pt"}
+    model_map = {
+        "F2F": "service/model/F2F.pt",
+        "C2C": "service/model/C2C.pt",
+        "A2A": "service/model/A2A.pt",
+    }
 
     if request.model_type not in model_map:
-        return {"status": "false", "message": "Invalid model_type. Please choose from F2F, C2C, A2A."}
-    
-    print(request.smiles)
-    print(request.model_type)
+        return {
+            "status": "false",
+            "message": "Invalid model_type. Please choose from F2F, C2C, A2A.",
+        }
 
     model_path = model_map[request.model_type]
-    
+
     # 調用預測函數，傳入 SMILES 列表
     try:
         predictions, smiles = predicting(request.smiles, model_path)
@@ -68,10 +66,7 @@ def predict_toxicity(request: PredictionRequest):
     # 構建 JSON 格式的返回值
     result = {
         "status": "success",
-        "data": {
-            "smiles": smiles,
-            "predictions": predictions
-        }
+        "data": {"smiles": smiles, "predictions": predictions},
     }
 
     return result
