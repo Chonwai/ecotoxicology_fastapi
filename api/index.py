6ad70230054effd1783bcb2f23c8e9376e22707f
Chonwai <chonwaiun@gmail.com>
2024-09-09 14:26:07 +0800

    Update: Updated the response.

--- api/index.py
+++ api/index.py
@@ -54,38 +54,38 @@ def hello_world():
     return {"message": "Hello World"}
 
 
-@app.post("/api/predict", response_class=ORJSONResponse)
-async def predict_toxicity(request: PredictionRequest):
-    model_map = {
-        "F2F": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/F2F-BvHdyhk9tyo7d5onoB9iHpwiFoogAq.pt",
-        "C2C": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/C2C-SpNj7ZlZAwHR6Nt4mrIkzEiUmLjhtQ.pt",
-        "A2A": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/A2A-L8ecvXA89xTLYL1BP8gDvmepPtw4mY.pt",
-    }
+# @app.post("/api/predict", response_class=ORJSONResponse)
+# async def predict_toxicity(request: PredictionRequest):
+#     model_map = {
+#         "F2F": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/F2F-BvHdyhk9tyo7d5onoB9iHpwiFoogAq.pt",
+#         "C2C": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/C2C-SpNj7ZlZAwHR6Nt4mrIkzEiUmLjhtQ.pt",
+#         "A2A": "https://bcoc8vpakuqoxkgl.public.blob.vercel-storage.com/A2A-L8ecvXA89xTLYL1BP8gDvmepPtw4mY.pt",
+#     }
 
-    if request.model_type not in model_map:
-        return {
-            "status": "false",
-            "message": "Invalid model_type. Please choose from F2F, C2C, A2A.",
-        }
+#     if request.model_type not in model_map:
+#         return {
+#             "status": "false",
+#             "message": "Invalid model_type. Please choose from F2F, C2C, A2A.",
+#         }
 
-    # 根據 model_type 選擇模型的 Blob URL
-    model_url = model_map[request.model_type]
-    model_name = f"{request.model_type}.pt"
+#     # 根據 model_type 選擇模型的 Blob URL
+#     model_url = model_map[request.model_type]
+#     model_name = f"{request.model_type}.pt"
 
-    # 下載模型（如果尚未下載）
-    model_path = download_model_if_not_exists(model_url, model_name)
+#     # 下載模型（如果尚未下載）
+#     model_path = download_model_if_not_exists(model_url, model_name)
 
-    # 調用預測函數，傳入 SMILES 列表
-    # try:
-    #     predictions, smiles = predicting(request.smiles, model_path)
-    # except Exception as e:
-    #     return {"error": f"Prediction failed: {str(e)}"}
+#     # 調用預測函數，傳入 SMILES 列表
+#     # try:
+#     #     predictions, smiles = predicting(request.smiles, model_path)
+#     # except Exception as e:
+#     #     return {"error": f"Prediction failed: {str(e)}"}
 
-    # 構建 JSON 格式的返回值
-    # result = {
-    #     "status": "success",
-    #     "data": {"smiles": smiles, "predictions": predictions},
-    # }
+#     # 構建 JSON 格式的返回值
+#     # result = {
+#     #     "status": "success",
+#     #     "data": {"smiles": smiles, "predictions": predictions},
+#     # }
 
-    # return result
-    return {"message": "Hello World"}
+#     # return result
+#     return {"message": "Hello World"}
